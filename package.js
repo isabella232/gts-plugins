a735606a45c3be0c1f6d138fe3a6d92e2ed70ac7
Matt Schott <ryltar@ku.edu>
2012-10-23 17:43:35 -0500

    Added global package

---
+++ package.js
@@ -0,0 +1,18 @@
+enyo.depends(
+	"AutoComplete",
+	"ConfirmDialog",
+	"DatePicker",
+	"DecimalInput",
+	"DividerDrawer",
+	"GAPI",
+	"InlineNotification",
+	"Joystick",
+	"Keyboard",
+	"LazyList",
+	"ProgressDialog",
+	"PrototypeExt",
+	"SelectedMenu",
+	"SelectorBar",
+	"TimePicker",
+	"ToggleBar"
+);
